ae6e72ebb8c48054986e43b4d551521f58f7a465
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-10-07 16:30:47 +0530

    fix:interview page issues

---
+++ app/interview/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="h-[50vh] mt-20 flex justify-center items-center text-3xl font-bold flex-col">
+      <h1>Interview Page</h1>
+      <p>Coming Soon</p>
+    </div>
+  );
+}
+
+export default Page;
