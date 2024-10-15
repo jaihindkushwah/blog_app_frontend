e3e42e8b3e3c6de1411b1060f6df9b9c8e8bacce
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-10-15 17:25:08 +0530

    feat:added search api with typesense

--- app/search/search/page.tsx
+++ app/search/search/page.tsx
@@ -1,6 +1,7 @@
 // import { searchBooks } from '../lib/typesense';
 
-import { searchBooks } from "@/lib/typesense";
+import { searchContents } from "@/lib/typesense";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -10,17 +11,19 @@ interface SearchPageProps {
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
   const query = searchParams.q || "";
-  const books = await searchBooks(query);
+  const contents = await searchContents(query);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Search Results for -: {query}</h1>
       <ul className="space-y-2">
-        {books?.map((book) => (
-          <li key={book.id} className="border p-2">
-            <h3 className="font-semibold">{book.title}</h3>
-            <p className="text-sm text-gray-600">{book.authors.join(", ")}</p>
-          </li>
+        {contents?.map((content) => (
+          <Link key={content.id} href={`/${content.titleId}`}>
+            <li className="border p-2">
+              <h3 className="font-semibold">{content.title}</h3>
+              <p className="text-sm text-gray-600">{content.description}</p>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
